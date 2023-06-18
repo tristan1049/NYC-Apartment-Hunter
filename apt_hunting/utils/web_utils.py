f972ca9c321190021cfa64ac670ab65ff16bbd53
Tristan Culp <tculp@mit.edu>
2023-06-17 20:05:24 -0400

    Add functionality to get all available listings

--- apt_hunting/utils/web_utils.py
+++ apt_hunting/utils/web_utils.py
@@ -1,5 +1,5 @@
 # REQUESTS CONSTANTS
-STREETEASY_URL = "https://streeteasy.com/for-rent/nyc"
+STREETEASY_URL = "https://streeteasy.com/for-rent/nyc?sort_by=listed_desc"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
@@ -11,3 +11,6 @@ PRICE_CLASS = 'price listingCard-priceMargin'
 BEDS_CLASS = 'listingDetailDefinitionsIcon listingDetailDefinitionsIcon--bed'
 BATHS_CLASS = 'listingDetailDefinitionsIcon listingDetailDefinitionsIcon--bath'
 SQ_FT_CLASS = 'listingDetailDefinitionsIcon listingDetailDefinitionsIcon--measure'
+
+def get_streeteasy_url(page_num=1):
+    return STREETEASY_URL + '&page={}'.format(str(page_num))
---
+++ apt_hunting/web.py
@@ -0,0 +1,57 @@
+import requests
+import time
+import random
+from bs4 import BeautifulSoup
+
+from utils.web_utils import get_streeteasy_url
+from utils.web_utils import HEADERS
+from utils.web_utils import LISTING_CLASS
+from utils.web_utils import BUILDING_CLASS
+from utils.web_utils import PRICE_CLASS
+from utils.web_utils import BEDS_CLASS
+from utils.web_utils import BATHS_CLASS
+from utils.web_utils import SQ_FT_CLASS
+
+def get_listings_one_page(page_num=1):
+    rv = []
+    response = requests.get(get_streeteasy_url(page_num), headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    listings = soup.find_all(class_=LISTING_CLASS) 
+    for listing in listings:
+        href = listing.a.get('href')
+        # Need to use find all as sometimes there are multiple lines, but only the last one has the building information
+        building_type = listing.find_all('p', class_=BUILDING_CLASS)[-1]
+        housing, district = building_type.string.strip().split(' in ')
+        address = listing.find('address').a.string.strip()
+        price = listing.find('span', class_=PRICE_CLASS).string.strip()
+        beds = listing.find('span', class_=BEDS_CLASS).next_sibling.next_sibling.string.strip()
+        baths = listing.find('span', class_=BATHS_CLASS).next_sibling.next_sibling.string.strip()
+        sq_ft = listing.find('span', class_=SQ_FT_CLASS)
+        if (sq_ft):
+            sq_ft = sq_ft.next_sibling.next_sibling.contents[0].strip()
+
+        rv.append([href, housing, district, address, price, beds, baths, sq_ft])
+
+    return rv
+
+# Generator that gets newest listings data for n=pages pages. If None, gets data from all pages
+def get_listings(pages=None):
+    # Get max number of pages
+    first_page = requests.get(get_streeteasy_url(1), headers=HEADERS) 
+    soup = BeautifulSoup(first_page.text, 'html.parser')
+    max_pages = int(soup.find_all('li', class_='page')[-1].a.string.strip())
+    time.sleep(2)
+
+    if pages:
+        if pages <= 0:
+            return
+        elif pages > max_pages:
+            pages = max_pages
+    elif pages == None:
+        pages = max_pages
+
+    for page in range(1, pages+1):
+        yield get_listings_one_page(page)
+        # Sleep for 4-8 seconds to not send too many requests at once
+        time.sleep(round(random.uniform(4, 8), 3))
